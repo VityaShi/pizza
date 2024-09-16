bcaf5fd6f701bd082c2f699660c1dbbbfea8e98d
FollowerOfDarkness <vitya.shitov.93@mail.ru>
2024-09-16 22:39:38 +0300

    16.09.24

--- src/views/home/ui/HomePage.tsx
+++ src/views/home/ui/HomePage.tsx
@@ -1,4 +1,10 @@
+import { Filters } from '@/widgets/filters'
+import { TopBar } from '@/widgets/top-bar'
+
 import { Container, Title } from '@/shared/ui'
+
+// import { Categories } from '../../../widgets/top-bar/ui/categories'
+// import { SortPopup } from '../../../widgets/top-bar/ui/sort-popup'
 
 export default function Home() {
 	return (
@@ -9,6 +15,17 @@ export default function Home() {
 					size='lg'
 					className='font-extrabold'
 				/>
+			</Container>
+			<TopBar />
+			<Container className='mt-10 pb-14'>
+				<div className='flex gap-[60px]'>
+					<div className='w-[250px]'>
+						<Filters />
+					</div>
+					<div className='flex-1'>
+						<div className='flex flex-col gap-16'>Product list</div>
+					</div>
+				</div>
 			</Container>
 		</>
 	)
